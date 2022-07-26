3ee5f2038e47a6a2b9de76c1a492f2adcc9ca0cf
Zx970040459 <970040459@qq.com>
2022-07-26 16:12:46 +0800

    20220726

--- mytree.c
+++ mytree.c
@@ -3,5 +3,48 @@
 int main()
 {
     printf("这是一个二叉树的测试程序！\n");
+    int level = 1;
+    BiTree T = NULL;
+    createBiTree(&T);
+
+    PreOrderTravers(T,level);
+
+    
     return 0;
+}
+
+/* 创建一棵二叉树 */
+void createBiTree(BiTree *T)
+{
+    char c;
+    printf("请输入要存储的数据！\n");
+    scanf("%c",&c);
+    if(' ' == c)
+    {
+        *T = NULL;
+    }
+    else{
+        *T = (BitNode*)malloc(sizeof(BitNode));
+        (*T)->data = c;
+        createBiTree(&(*T)->lchild);
+        createBiTree(&(*T)->rchild);
+    }
+}
+/* 访问二叉树结点的具体操作 */
+void visit(char c,int level)
+{
+    printf("%c位于第%d层\n",c,level);
+}
+
+/* 遍历二叉树 */
+void PreOrderTravers(BiTree T,int level)
+{
+    printf("遍历函数测试！\n");
+    if(T != NULL)
+    {
+        printf("data:%c",T->data);
+        visit(T->data,level);
+        PreOrderTravers(T->lchild,level+1);
+        PreOrderTravers(T->rchild,level+1);
+    }
 }
--- mytree.h
+++ mytree.h
@@ -3,11 +3,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_TREE_SIZE 100
+
+typedef char ElemType;  //int类型重命名
 typedef struct Node
 {
     int data;
     struct Node *treeLeft;
     struct Node *treeRight;
 }tree_t;
+
+/* 树的双亲孩子表示法 */
+
+/* 定义孩子结点类型结构体 */
+typedef struct CTNode
+{
+    int child; //孩子结点的下标
+    struct CTNode *next;//指向下一个孩子结点的指针
+}*ChildPtr;
+
+/* 表头结构 */
+
+typedef struct 
+{
+    ElemType data; //存放在树中结点的数据
+    int parent;    //指向双亲的下标
+    ChildPtr firstchild;//指向第一个孩子的指针
+}CTBox;
+/* 树结构 */
+typedef struct 
+{
+    CTBox nodes[MAX_TREE_SIZE];//结点数组
+    int r,n;
+};
+
+/* 二叉树的存储结构 */
+typedef struct BitNode
+{
+    ElemType data;
+    struct BitNode *lchild,*rchild;
+}BitNode,*BiTree;
+
+void createBiTree(BiTree *T);
+void PreOrderTravers(BiTree T,int level);
+void visit(char c,int level);
 
 #endif
