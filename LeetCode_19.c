b040ff12d7facfb8476ba790f6390a7559169799
Zx970040459 <970040459@qq.com>
2022-07-28 17:06:03 +0800

    LeetCode_19

--- LeetCode_19.c
+++ LeetCode_19.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* 删除链表的倒数第N个结点 */
+/* LeetCode 第19题：删除链表的倒数第N个结点 */
 typedef struct ListNode 
 {
     int val;
     struct ListNode *next;
 }ListNode;
-
-int main()
-{
-    return 0;
-}
-
+/* 获取链表的长度 */
 int getLength(struct ListNode* head) {
     int length = 0;
     while (head) {
@@ -20,8 +15,9 @@ int getLength(struct ListNode* head) {
     }
     return length;
 }
-
-struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+/* 删除链表倒数第N个结点的实现函数：方法1 */
+struct ListNode* removeNthFromEnd1(struct ListNode* head, int n) {
+    /* 创建一个哑结点 */
     struct ListNode* dummy = malloc(sizeof(struct ListNode));
     dummy->val = 0;
     dummy->next = head;
@@ -35,3 +31,43 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
     free(dummy);
     return ans;
 }
+
+/* 方法2：双指针 */
+struct ListNode* removeNthFromEnd2(struct ListNode* head, int n) 
+{
+    /* ①创建一个哑结点 */
+    ListNode *dummy = (ListNode*)malloc(sizeof(ListNode));
+    /* ②哑结点指向链表的头结点 */
+    dummy->next = head;
+    /* ③创建两个遍历指针，指向dummy */
+    ListNode *pPrev = dummy;
+    ListNode *pMove = dummy;
+    /* ④后移pMove指针，直到pMove和pPrev指针之间相差n个结点 */
+    for(int i = 0;i<n+1;i++)
+    {
+        pMove = pMove->next;
+    }
+    /* ⑤移动双指针，直到pMove指针指向NULL，即链表遍历结束 */
+    while (pMove != NULL)
+    {
+        pPrev = pPrev->next;
+        pMove = pMove->next;
+    }
+    /* ⑥定义要删除的结点 */
+    ListNode *delNode = pPrev->next;
+    /* ⑥-1删除操作 */
+    pPrev->next = delNode->next;
+    free(delNode);
+    
+    ListNode *retNode = dummy->next;
+    free(dummy);
+    
+    return retNode;
+}
+
+
+int main()
+{
+    return 0;
+}
+
